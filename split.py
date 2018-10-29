392e68c9e1f43cef686fbf0e4bc19844faefe1d6
assassinatorsden312 <40004295+assassinatorsden312@users.noreply.github.com>
2018-10-29 21:19:09 +0530

    Update Split.py

--- split.py
+++ split.py
@@ -1,1 +1,25 @@
 
+
+#Program 3
+
+
+#tuple1=()
+templist2=[]
+#string=input("Enter the elements of the list (comma separated)")
+#templist1=string.split(",")
+#templist2=string
+
+
+
+j=0
+for i in string:
+    if(string[i]==",")or(string[i:]):
+        templist2.append(string[j:int(i)])
+
+
+"""        
+list1+=templist1
+tuple1+=tuple(templist1)
+print(list1)
+print(tuple1)
+"""
