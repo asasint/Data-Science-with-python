a2232d4491a64b1a7baab621ee7cd761880de71d
premmody312 <32757616+premmody312@users.noreply.github.com>
2018-10-29 21:11:39 +0530

    Emptied

--- 1714031_Prem_comparisoncharts.py
+++ 1714031_Prem_comparisoncharts.py
@@ -1,22 +1,4 @@
-import matplotlib.pyplot as plt
-import pandas as pd
-import numpy as np
-df=pd.read_csv("population_csv.csv",skiprows=[0],names=["CountryName","CountryCode","Year","Value"])
 
-df1=df[df.CountryName=="India"]
-df2=df[df.CountryName=="Zimbabwe"]
-df3=df[df.CountryName=="Mexico"]
-df4=df[df.CountryName=="Uzbekistan"]
-df5=df[df.CountryName=="United States"]
-
-plt.plot(df1.Year,df1.Value,label="India")
-plt.plot(df2.Year,df2.Value,label="Zimbabwe")
-plt.plot(df3.Year,df3.Value,label="Mexico")
-plt.plot(df4.Year,df4.Value,label="Uzbekistan")
-plt.plot(df5.Year,df5.Value,label="United States")
-
-plt.legend(loc="upper right")
-plt.show()
 
 
 
